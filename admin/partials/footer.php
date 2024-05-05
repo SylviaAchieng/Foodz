eb1ca4cfc976f1c6d26b8a0cc3e57bcb45bdef51
SylviaAchieng <achiengsylvia157@gmail.com>
2024-05-05 19:57:21 +0300

    update

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <div class="footer">
             <div class="wrapper">
-                <p class="text-center">2023 All rights reserved, Food House. Developed By - <a href="#"> Studies group members</a></p>
+                <p class="text-center">2023 All rights reserved, Food House. Developed By - <a href="#"> Sylvia@A</a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
